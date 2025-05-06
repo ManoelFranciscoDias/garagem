8cc8577168efc2677dab95195b478ee02f5846eb
ManoelFranciscoDias <manoel2007dias@gmail.com>
2025-05-06 15:06:24 -0300

    feat: criação da api cor

--- config/urls.py
+++ config/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter 
-from core.views import AcessorioViewSet
+from core.views import AcessorioViewSet, CorViewSet
 
 router = DefaultRouter()
 
-router.register(r'categorias/', AcessorioViewSet)
+router.register(r'categorias', AcessorioViewSet)
+router.register(r'cor', CorViewSet)
 
 
 urlpatterns = [
